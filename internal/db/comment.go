05bceaef5966cc128697999a9142a29fd73e2d9f
Busayo Famutimi <famutimi.busayo@gmail.com>
2023-02-16 12:15:21 +0100

    Impl DeleteComment and UpdateComment at the repository

--- internal/db/comment.go
+++ internal/db/comment.go
@@ -59,3 +59,36 @@ func (d *Database) PostComment(ctx context.Context, newComment comment.Comment) 
 
 	return newComment, nil
 }
+
+func (d *Database) DeleteComment(ctx context.Context, id string) error {
+	_, err := d.Client.ExecContext(ctx, `DELETE FROM comments WHERE id = $1`, id)
+
+	if err != nil {
+		return fmt.Errorf("failed to delete comment from database: %w", err)
+	}
+
+	return nil
+}
+
+func (d *Database) UpdateComment(ctx context.Context, id string, updateComment comment.Comment) (comment.Comment, error) {
+	commentRow := CommentRow{
+		ID: id,
+		Slug: sql.NullString{String: updateComment.Slug, Valid: true},
+		Author: sql.NullString{String: updateComment.Author, Valid: true},
+		Body: sql.NullString{String: updateComment.Body, Valid: true},
+	}
+
+	rows, err := d.Client.NamedQueryContext(ctx,
+		`UPDATE comments SET slug = :slug, author = :author, body = :body WHERE id = :id`, commentRow,
+	)
+
+	if err != nil {
+		return comment.Comment{}, fmt.Errorf("failed to update comment: %w", err)
+	}
+
+	if err := rows.Close(); err != nil {
+		return comment.Comment{}, fmt.Errorf("failed to close rows: %w", err)
+	}
+
+	return convertCommentRowToComment(commentRow), nil
+}
